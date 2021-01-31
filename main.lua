7cb2892b1a21c31b74e47ba7ceaa4da81302178e
Toasted-Rice <javo9205@colorado.edu>
2021-01-31 02:11:58 -0700

    Changed main slightly, moved characters to assets

--- main.lua
+++ main.lua
@@ -1,22 +1,22 @@
 require 'src/Dependencies'
 
 local OS = love.system.getOS()
-local PLATFORM = (OS == 'Android' or OS == 'iOS') and 'Mobile' or 'Desktop'
+local MOBILE = (OS == 'Android' or OS == 'iOS') and true or false
 local SCREEN_WIDTH, SCREEN_HEIGHT = love.window.getDesktopDimensions()
 local PC_DEFAULT_WIDTH, PC_DEFAULT_HEIGHT = 1280, 720
 local MIN_RENDER_WIDTH, MIN_RENDER_HEIGHT  = 512, 288
 local MAX_RENDER_WIDTH, MAX_RENDER_HEIGHT = 720, 480
 
 local fullscreen = false
+local landscape = true
 local windowWidth, windowHeight = 1,1
+local virtualWidth, virtualHeight = 1,1
 local screenPosX, screenPosY = nil, nil
 local aspectRatio = 1.5
 local flagScreenResized = false
 
-local background = love.graphics.newImage('assets/background.jpg')
-
 function love.load()
-    if (PLATFORM == 'Mobile' or fullscreen) then
+    if (MOBILE or fullscreen) then
          windowWidth, windowHeight = SCREEN_WIDTH, SCREEN_HEIGHT
     else windowWidth, windowHeight = PC_DEFAULT_WIDTH, PC_DEFAULT_HEIGHT
     end
@@ -27,12 +27,13 @@ function love.load()
 end
 
 function love.update(dt)
-
+    print(love.window.hasMouseFocus())
 end
 
 function love.draw()
     push:start()
-    love.graphics.draw(background, 0, 0)
+    love.graphics.setColor(1,1,1)
+    love.graphics.rectangle('fill',0,0,virtualWidth,virtualHeight)
     push:finish()
 end
 
@@ -50,22 +51,35 @@ function love.mousefocus()
     end
 end
 
+function love.mousepressed()
+    print(love.mouse.getPosition())
+end
+
+function love.keypressed()
+    if flagScreenResized then
+        initScreen()
+        love.window.setPosition(screenPosX, screenPosY)
+        flagScreenResized = false
+    end
+end
+
 -- (Re)initializes the screen on function call.
 -- Supports aspect ratios from 5:2 to 16:15
 function initScreen()
     aspectRatio = windowWidth / windowHeight
-    local virtualWidth  = aspectRatio > 1 and MIN_RENDER_WIDTH or MIN_RENDER_HEIGHT
-    local virtualHeight = aspectRatio > 1 and MIN_RENDER_HEIGHT or MIN_RENDER_WIDTH
-
-    if aspectRatio > 1.5 then
-         virtualWidth  = math.min(MIN_RENDER_HEIGHT * aspectRatio, MAX_RENDER_WIDTH)
-    elseif aspectRatio < 0.667 then
-         virtualHeight = math.min(MIN_RENDER_HEIGHT / aspectRatio, MAX_RENDER_WIDTH)
-    elseif aspectRatio < 1 then
-         virtualWidth  = math.min(MIN_RENDER_WIDTH  * aspectRatio, MAX_RENDER_HEIGHT)
-    else virtualHeight = math.min(MIN_RENDER_WIDTH  / aspectRatio, MAX_RENDER_HEIGHT)
-    end
+    landscape = aspectRatio > 1 and true or false
     
+    local longSide  = MIN_RENDER_WIDTH
+    local shortSide = MIN_RENDER_HEIGHT
+    local inverseRatio = 1 / aspectRatio
+    if aspectRatio > 1.5 or aspectRatio < 0.667 then
+         longSide  = math.min(MIN_RENDER_HEIGHT * (landscape and aspectRatio or inverseRatio), MAX_RENDER_WIDTH)
+    else shortSide = math.min(MIN_RENDER_WIDTH  * (landscape and inverseRatio or aspectRatio), MAX_RENDER_HEIGHT) end
+
+    if landscape then
+         virtualWidth,virtualHeight = longSide, shortSide
+    else virtualWidth,virtualHeight = shortSide, longSide end
+
     push:setupScreen(virtualWidth, virtualHeight, windowWidth, windowHeight,{
         vsync = true,
         fullscreen = fullscreen,
